e4784af94eb451035f70dd9c851a46ce236e6395
limobai2333 <65392605+limobai2333@users.noreply.github.com>
2020-11-01 11:30:37 +0800

    Update UnicomAutoGetCoin.py

--- UnicomAutoGetCoin.py
+++ UnicomAutoGetCoin.py
@@ -76,11 +76,6 @@ if __name__ == '__main__':
 
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    data = timestamp+sys.argv[1604200385174]
+    data = timestamp+sys.argv[1]
 
     user.sign(data)
-    
-    
-    
-    
-    
